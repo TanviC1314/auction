fbd7d385fd4029c60d15e83b09ead99f66bb2238
VinaySavla <savlavinay77@gmail.com>
2025-01-06 16:22:41 +0530

    added auto refresh every 5 mins

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import TeamsPage from "./pages/TeamsPage";
@@ -8,6 +8,15 @@ import UnsoldPlayersPage from "./pages/UnsoldPlayersPage";
 import YetToAuctionPage from "./pages/YetToAuctionPage";
 
 const App = () => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      window.location.reload();
+    }, 300000); // Refresh every 5 minutes (300,000 ms)
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Router>
       <Routes>
